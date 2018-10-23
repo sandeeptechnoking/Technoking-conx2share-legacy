f43940797d663da14b56917df20736f8b9f9a57c
sandeep <sandeep.k@technoking.in>
2018-10-23 12:51:55 +0530

    changes final
    (cherry picked from commit 88f33e81fecb2ba37b522d7359502c9882e1e173)

--- app/src/main/java/com/conx2share/conx2share/ui/livestream/StreamHomeActivity.java
+++ app/src/main/java/com/conx2share/conx2share/ui/livestream/StreamHomeActivity.java
@@ -64,7 +64,7 @@ public class StreamHomeActivity extends BaseAppCompatActivity {
 
     private static final String EXTRAS_IS_GROUP = "from_group_key";
     private static final String EXTRAS_STREAMER_ID = "streamer_id_key";
-    private String check;
+    private String checkii;
 
     @BindView(R.id.stream_toolbar)
     Toolbar toolbar;
